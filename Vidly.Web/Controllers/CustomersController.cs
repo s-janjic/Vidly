025dd4fb029a398254d4005ac09814f334a49c0b
Stefan <janjicstefan163@gmail.com>
2021-07-19 21:45:18 +0200

    Added validation summary and changed position for memership type field

--- Vidly.Web/Controllers/CustomersController.cs
+++ Vidly.Web/Controllers/CustomersController.cs
@@ -29,6 +29,7 @@ namespace Vidly.Web.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
