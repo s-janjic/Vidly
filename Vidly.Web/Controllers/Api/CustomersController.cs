7094b37b8d8956d24574cd25dd914e267fa446be
Stefan <janjicstefan163@gmail.com>
2021-07-24 18:07:40 +0200

    New Api for 'customers' - Implemented to get all customers

---
+++ Vidly.Web/Controllers/Api/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Web.Models;
+
+namespace Vidly.Web.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+    }
+}
