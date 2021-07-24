c078c20bac517fed5b65bcaf6f05779784ce0454
Stefan <janjicstefan163@gmail.com>
2021-07-24 19:46:12 +0200

    Setup Auto mapper - customer to dto - dto to customer

---
+++ Vidly.Web/App_Start/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Web.Dtos;
+using Vidly.Web.Models;
+
+namespace Vidly.Web.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // map customer to customerdto
+            Mapper.CreateMap<Customer, CustomerDto>();
+
+            // map customerdto to customer
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
--- Vidly.Web/Global.asax.cs
+++ Vidly.Web/Global.asax.cs
@@ -2,6 +2,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using Vidly.Web.App_Start;
 
 namespace Vidly.Web
 {
@@ -9,6 +11,7 @@ namespace Vidly.Web
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(config => config.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
