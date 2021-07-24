697632a607463fab0eea22bc6c458af330cec3ca
Stefan <janjicstefan163@gmail.com>
2021-07-24 19:12:34 +0200

    Fix correct response for creating customer if request is not valid - New Api for updating existing customer - Created mapper to replace old customer with new data

--- Vidly.Web/Controllers/Api/CustomersController.cs
+++ Vidly.Web/Controllers/Api/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Net;
 using System.Web.Http;
+using Vidly.Web.Mappers;
 using Vidly.Web.Models;
 
 namespace Vidly.Web.Controllers.Api
@@ -43,13 +44,35 @@ namespace Vidly.Web.Controllers.Api
         {
             // check if sent request is valid
             if (!ModelState.IsValid)
-                throw new HttpResponseException(HttpStatusCode.BadGateway);
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
 
             // add customer to the context and save changes
             _context.Customers.Add(customer);
             _context.SaveChanges();
 
             return customer;
+        }
+
+        // edit (update) existing customer in Db
+        // PUT /api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            // check if request is valid
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            // if 'id' is not valid from request
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            // all good, update (replacing old data with the new from request)
+            // map new customer from request to old customer
+            MappersHelper.MapNewCustomer(customerInDb, customer);
+
+            _context.SaveChanges();
         }
     }
 }
