5210b4a08d18ee2a1a9f5d3576137173603ea293
Stefan <janjicstefan163@gmail.com>
2021-08-07 09:36:14 +0200

    New API: - GetMovies - returns all movies - GetMovie - returns movie by id

---
+++ Vidly.Web/Controllers/Api/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.Web.Dtos;
+using Vidly.Web.Models;
+
+namespace Vidly.Web.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/movies
+        public IHttpActionResult GetMovies()
+        {
+            // returning the list of movies and map to DTO
+            return Ok(_context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>));
+        }
+
+        // GET /api/movies/1
+        public IHttpActionResult GetMovie(int id)
+        {
+            // try to get movie from DB
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            // check if not found
+            if (movie == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+    }
+}
