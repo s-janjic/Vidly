566546bccf6211506c813c04ad7b3fa2f57d47a5
Stefan <janjicstefan163@gmail.com>
2021-08-07 09:55:04 +0200

    Mapping profile fixed

--- Vidly.Web/App_Start/MappingProfile.cs
+++ Vidly.Web/App_Start/MappingProfile.cs
@@ -16,13 +16,15 @@ namespace Vidly.Web.App_Start
             Mapper.CreateMap<Customer, CustomerDto>();
 
             // map customerdto to customer
-            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
 
             // map movie to moviedto
             Mapper.CreateMap<Movie, MovieDto>();
 
             // map moviedto to movie
-            Mapper.CreateMap<MovieDto, Movie>();
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
